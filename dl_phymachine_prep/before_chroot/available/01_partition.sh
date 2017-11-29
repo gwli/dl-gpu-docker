002cc544c12d734e1f099b8f292e4ab72ef876a1
victor <vili@nvidia.com>
2017-11-29 16:26:12 +0800

    fix syntax error

--- dl_phymachine_prep/before_chroot/available/01_partition.sh
+++ dl_phymachine_prep/before_chroot/available/01_partition.sh
@@ -5,15 +5,14 @@
 ###
 
 
-sudo {
+sudo  true
 # Remove each partition
 echo "delete existing paritions"
 for v_partition in $(parted -s $DL_PYM_DEVICE print|awk '/^ / {print $1}')
 do
-   parted -s $DL_PYM_DEVICE rm ${v_partition}
+  sudo parted -s $DL_PYM_DEVICE rm ${v_partition}
 done
 
 # Create single partition
-parted -s $DL_PYM_DEVICE mkpart primary 0 ${v_disk}
+sudo parted -s $DL_PYM_DEVICE mkpart primary 0 ${v_disk}
 
-}
--- dl_phymachine_prep/dl_init/after_chroot/available/01-basic-packages.sh
+++ dl_phymachine_prep/dl_init/after_chroot/available/01-basic-packages.sh
@@ -1,9 +1,8 @@
 echo "install vim and sshserver"
-sudo {
-apt update && apt install  -y vim openssh-server git wget curl cmake pyhon-dev build-essential
+sudo apt update && apt install  -y vim openssh-server git wget curl cmake python-dev build-essential
 
-curl -O https://bootstrap.pypa.io/get-pip.py && \
+sudo curl -O https://bootstrap.pypa.io/get-pip.py && \
     python get-pip.py && \
     rm get-pip.py
 
-}
+
