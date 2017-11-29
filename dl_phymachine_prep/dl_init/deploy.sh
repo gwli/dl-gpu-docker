6de27c74807d9a0713b5a7b1a7404b2eb1428306
victor <vili@nvidia.com>
2017-11-29 16:14:07 +0800

    finish the basic function

--- dl_phymachine_prep/dl_init/deploy.sh
+++ dl_phymachine_prep/dl_init/deploy.sh
@@ -1,4 +1,6 @@
-for i in after_chroot/*;
+echo "after chroot"
+sudo true
+for i in after_chroot/$1/*;
 do 
   echo "execute... $i"
   $i
--- dl_phymachine_prep/install.sh
+++ dl_phymachine_prep/install.sh
@@ -1,18 +1,28 @@
-export PARTITION=$1
 
-for i in before_chroot/*;
+export DL_PYM_DEVICE=/dev/sda
+export DL_PYM_PARTITION_NUM=1
+export DL_PYM_OFFLINE_PKGS_DIR=offline_pkgs
+export DL_PYM_MOUNTPOINT=/mnt/DL$DL_PYM_PARTITION_NUM
+export DL_PYM_INIT_DIR=/dl_init
+export DL_PYM_INIT_PKG=./dl_init
+export DL_PYM_ROOTFS=ubuntu160403rootfs_2017_11_29_nv.tgz
+export DL_PYM_CONFIG=default
+
+sudo true
+
+for i in before_chroot/$DL_PYM_CONFIG/*;
 do 
   echo "excute ... $i"
-  . $i
+  #. $i
 done   
 
+exit
+sudo mount $DL_PYM_DEVICE$DL_PYM_PARTITION_NUM $DL_PYM_MOUNTPOINT
+sudo mount -t proc /proc   $DL_PYM_MOUNTPOINT/proc
+sudo mount --rbind /sys    $DL_PYM_MOUNTPOINT/sys
+sudo mount --make-rslave   $DL_PYM_MOUNTPOINT/sys
+sudo mount --rbind /dev    $DL_PYM_MOUNTPOINT/dev
+sudo mount --make-rslave   $DL_PYM_MOUNTPOINT/dev
 
-mount /dev/$PARTITION /mnt/$PARTITION
-mount -t proc /proc /mnt/$PARTITION/proc
-mount --rbind /sys /mnt/$PARTITION/sys
-mount --make-rslave /mnt/$PARTITION/sys
-mount --rbind /dev /mnt/$PARTITION/dev
-mount --make-rslave /mnt/$PARTITION/dev
-
-chroot /mnt/$PARTITION /dl_prep/deploy.sh
+sudo chroot $DL_PYM_MOUNTPOINT $DL_PYM_INIT_DIR/deploy.sh $DL_PYM_CONFIG
 
