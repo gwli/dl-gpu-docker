2efbd8d8b623bae14bf1f33a3ee713bd6d01b4c2
victor <vili@nvidia.com>
2018-01-12 11:02:17 +0800

    update Docker_hpc 	modified:   Dockerfile.hpc 	modified:   build_hpc_docker.sh

--- build_hpc_docker.sh
+++ build_hpc_docker.sh
@@ -1,4 +1,4 @@
-tagname="nvdevtools/hpc-cuda-$1:latest -f Dockerfile.hpc .
+tagname="nvdevtools/hpc-cuda-$1:latest"
 nvidia-docker build -t $tagname -f Dockerfile.hpc . && \
 docker push  $tagname
 
